5f0f196ac80995642963e4f626112e127f0ef7a9
dauitsuragan002 <78667630+dauitsuragan002@users.noreply.github.com>
2023-06-29 04:55:12 +0600

    Update TTSSub2.py

--- TTS/TTS-google-colab/TTSSub2.py
+++ TTS/TTS-google-colab/TTSSub2.py
@@ -2,19 +2,11 @@ import asyncio
 import edge_tts
 import fileinput
 
-VOICE = "kk-KZ-DauletNeural" #ru-RU-SvetlanaNeural/kk-KZ-AigulNeural/ru-Ru-DmitryNeural
+VOICE = "kk-KZ-DauletNeural" #ru-RU-SvetlanaNeural/kk-KZ-AigulNeural/ru-Ru-DmitryNeural kk-KZ-DauletNeural en-US-GuyNeural
 OUTPUT_FILE = "test.mp3"
 WEBVTT_FILE = "test.vtt"
 
-async def _main() -> None:
-    # Считываем текст из файла
-    text = ""
-    with fileinput.input(files=("example.txt"), openhook=fileinput.hook_encoded("utf-8")) as file:
-        for line in file:
-            text += line
-    text = text.strip()
-    
-    # Генерируем аудио файл
+async def generate_audio(text):
     communicate = edge_tts.Communicate(text, VOICE)
     submaker = edge_tts.SubMaker()
     with open(OUTPUT_FILE, "wb") as file:
@@ -24,13 +16,16 @@ async def _main() -> None:
             elif chunk["type"] == "WordBoundary":
                 submaker.create_sub((chunk["offset"], chunk["duration"]), chunk["text"])
 
-    # Генерируем файл с субтитрами в формате WebVTT
     with open(WEBVTT_FILE, "w", encoding="utf-8") as file:
         file.write(submaker.generate_subs())
 
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(_main())
-    finally:
-        loop.close()
+async def main():
+    text = ""
+    with fileinput.input(files=("/content/12.txt"), openhook=fileinput.hook_encoded("utf-8")) as file:
+        for line in file:
+            text += line
+    text = text.strip()
+    
+    await generate_audio(text)
+
+asyncio.create_task(main())
