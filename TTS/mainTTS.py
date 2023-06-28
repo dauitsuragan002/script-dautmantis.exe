5e69a2c932d536ce1e62d0e7fbd30a393aae6ed2
dauitsuragan002 <78667630+dauitsuragan002@users.noreply.github.com>
2023-06-28 19:31:30 +0600

    Update mainTTS.py

--- TTS/mainTTS.py
+++ TTS/mainTTS.py
@@ -2,35 +2,21 @@ import asyncio
 import edge_tts
 import fileinput
 
-VOICE = "kk-KZ-DauletNeural"  # ru-RU-SvetlanaNeural/kk-KZ-AigulNeural/ru-Ru-DmitryNeural
+VOICE = "kk-KZ-DauletNeural" 
 OUTPUT_FILE = "test.mp3"
-WEBVTT_FILE = "test.vtt"
 
-async def generate_audio(text):
-    # Генерируем аудио файл
+async def _main() -> None:
+    text = ''
+    with fileinput.input(files=('Файл атауын расширениесімен көрсетіңіз.txt'), openhook=fileinput.hook_encoded("utf-8")) as file:
+        for line in file:
+            text += line
+    text = text.strip()
     communicate = edge_tts.Communicate(text, VOICE)
-    submaker = edge_tts.SubMaker()
-    with open(OUTPUT_FILE, "wb") as file:
-        async for chunk in communicate.stream():
-            if chunk["type"] == "audio":
-                file.write(chunk["data"])
-            elif chunk["type"] == "WordBoundary":
-                submaker.create_sub((chunk["offset"], chunk["duration"]), chunk["text"])
-
-    # Генерируем файл с субтитрами в формате WebVTT
-    with open(WEBVTT_FILE, "w", encoding="utf-8") as file:
-        file.write(submaker.generate_subs())
-
-def main():
-    # Укажите текст напрямую
-    text = "Ваш текст здесь"
-
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    try:
-        loop.run_until_complete(generate_audio(text))
-    finally:
-        loop.close()
+    await communicate.save(OUTPUT_FILE)
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(_main())
+    finally:
+        loop.close()
