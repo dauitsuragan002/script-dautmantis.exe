515b55d6de55f7928dd21c841ac20b83fc2ac641
dauitsuragan002 <78667630+dauitsuragan002@users.noreply.github.com>
2023-07-05 22:51:26 +0600

    Update mainTTSSub2.py

--- TTS/edge-tts/mainTTSSub2.py
+++ TTS/edge-tts/mainTTSSub2.py
@@ -1,15 +1,18 @@
 import asyncio
 import edge_tts
+import fileinput
 
 VOICE = "kk-KZ-DauletNeural" #ru-RU-SvetlanaNeural/kk-KZ-AigulNeural/ru-Ru-DmitryNeural
 OUTPUT_FILE = "test.mp3"
 WEBVTT_FILE = "test.vtt"
 
 async def _main() -> None:
-    # Укажите текст напрямую
-    text = "Осында өзіңіздің текстіңізді жазсаңыз болады"
-
-    # Генерируем аудио файл
+    text = ""
+    with fileinput.input(files=("123.txt"), openhook=fileinput.hook_encoded("utf-8")) as file: #Дайын тексттік файлдың аты мен орнын нұсқаймыз
+        for line in file:
+            text += line
+    text = text.strip()
+    
     communicate = edge_tts.Communicate(text, VOICE)
     submaker = edge_tts.SubMaker()
     with open(OUTPUT_FILE, "wb") as file:
@@ -19,7 +22,6 @@ async def _main() -> None:
             elif chunk["type"] == "WordBoundary":
                 submaker.create_sub((chunk["offset"], chunk["duration"]), chunk["text"])
 
-    # Генерируем файл с субтитрами в формате WebVTT
     with open(WEBVTT_FILE, "w", encoding="utf-8") as file:
         file.write(submaker.generate_subs())
 
