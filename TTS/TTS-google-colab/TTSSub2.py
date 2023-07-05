2c332d59e2dcdf798bd591c3a98812ea7a51a7a7
dauitsuragan002 <78667630+dauitsuragan002@users.noreply.github.com>
2023-07-05 22:47:43 +0600

    Update TTSSub2.py

--- TTS/TTS-google-colab/TTSSub2.py
+++ TTS/TTS-google-colab/TTSSub2.py
@@ -3,10 +3,31 @@
 import asyncio
 import edge_tts
 import fileinput
+import os
 
 VOICE = "kk-KZ-DauletNeural" #ru-RU-SvetlanaNeural/kk-KZ-AigulNeural/ru-Ru-DmitryNeural kk-KZ-DauletNeural en-US-GuyNeural
 OUTPUT_FILE = "test.mp3"
 WEBVTT_FILE = "test.vtt"
+SRT_FILE = "test.srt"
+
+def vtt_to_srt(vtt_file, srt_file):
+    with open(vtt_file, "r", encoding="utf-8") as vtt:
+        vtt_content = vtt.read()
+
+    srt_content = vtt_content.replace("WEBVTT\n\n", "")
+
+    # Разбиваем содержимое файла VTT на субтитры
+    vtt_subtitles = srt_content.split("\n\n")
+
+    # Создаем содержимое файла SRT
+    srt_content = ""
+    for i, vtt_subtitle in enumerate(vtt_subtitles, start=1):
+        if vtt_subtitle.strip():  # Проверяем, что субтитр не пустой
+            srt_content += f"{i}\n{vtt_subtitle}\n\n"
+
+    with open(srt_file, "w", encoding="utf-8") as srt:
+        srt.write(srt_content)
+
 
 async def generate_audio(text):
     communicate = edge_tts.Communicate(text, VOICE)
@@ -17,18 +38,21 @@ async def generate_audio(text):
                 file.write(chunk["data"])
             elif chunk["type"] == "WordBoundary":
                 submaker.create_sub((chunk["offset"], chunk["duration"]), chunk["text"])
+                if not chunk["text"].strip() and not communicate.has_pending_data():
+                    submaker.remove_last_sub()
 
     with open(WEBVTT_FILE, "w", encoding="utf-8") as file:
         file.write(submaker.generate_subs())
 
+    vtt_to_srt(WEBVTT_FILE, SRT_FILE)
+    os.remove(WEBVTT_FILE)
+
 async def main():
     text = ""
-    with fileinput.input(files=("/content/12.txt"), openhook=fileinput.hook_encoded("utf-8")) as file:  #Дайын тексттік файлдың аты мен орнын нұсқаймыз
+    with fileinput.input(files=("/content/Жалпаюға_көмектесетін_бір_жаттығу_ТАРТЫЛУДЫ_тез_үйрену.txt"), openhook=fileinput.hook_encoded("utf-8")) as file: #Дайын тексттік файлдың аты мен орнын нұсқаймыз
         for line in file:
             text += line
     text = text.strip()
 
     await generate_audio(text)
-
-
 asyncio.create_task(main())
